a197182df4916eae3825ee65409767a9e6d5bc0a
tunjiNg01 <nahtlosng@gmail.com>
2023-08-09 19:21:04 +0100

    init

---
+++ datingapp/datingapp/datingappApp.swift
@@ -0,0 +1,17 @@
+//
+//  datingappApp.swift
+//  datingapp
+//
+//  Created by MACBOOK PRO on 09/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct datingappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
